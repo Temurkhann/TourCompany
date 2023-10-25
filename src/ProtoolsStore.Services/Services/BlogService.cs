e04cdcab2dc14f8f40e36b41ec82d7861e6c06bd
Temurkhann <temurxonshodiyev4@gmail.com>
2023-10-25 05:12:01 +0500

    changes

---
+++ src/ProtoolsStore.Services/Services/BlogService.cs
@@ -0,0 +1,31 @@
+﻿using ProtoolsStore.Data.IRepositories;
+using ProtoolsStore.Services.DTOs;
+using ProtoolsStore.Services.Interfaces;
+using ProtoolsStore.Services.ViewModels.Blogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoolsStore.Services.Services
+{
+    public class BlogService : IBlogService
+    {
+        private readonly IRepository
+        public Task<BlogViewModel> CreateAsync(BlogForCreationDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BlogViewModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogViewModel> GetAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
